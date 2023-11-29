1b66dcfa14fee56752bf9e8fd99e947380e3dec4
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-11-29 01:07:59 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./errors/bad-request-error";
 export * from "./errors/confilct-error";
 export * from "./errors/custom-error";
+export * from "./errors/forbidden-error";
 export * from "./errors/not-found-error";
 export * from "./errors/request-validation-error";
 export * from "./errors/unauthorized-error";
